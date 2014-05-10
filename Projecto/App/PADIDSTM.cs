64637d137c6935ab42b7d80236f2162db7fb8383
Duolasa <duolasa@gmail.com>
2014-05-10 15:48:51 +0100

    Re-naming of stuff

---
+++ Projecto/App/PADIDSTM.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace PADIDSTM
+{
+  class PADIDSTM
+    {
+
+      static IMaster masterServer;
+      static ServerHashTable dataServersPorts;
+
+        public static bool Init()
+        {
+            return true;
+        }
+
+        public bool TxBegin()
+        {
+            return true;
+        }
+        public bool TxCommit()
+        {
+            return true;
+        }
+        public bool TxAbort()
+        {
+            return true;
+        }
+        public bool Status()
+        {
+            return true;
+        }
+        public bool Fail(string url)
+        {
+            return true;
+        }
+        public bool Freeze(string url)
+        {
+            return true;
+        }
+        public bool Recover(string url)
+        {
+            return true;
+        }
+
+        static void RequestHash()
+        {
+          dataServersPorts = masterServer.requestHashTable();
+        }
+
+        static void CreatePadInt()
+        {
+          Random rnd = new Random();
+          int num = rnd.Next(0, 666);
+          string url = dataServersPorts.getServerByPadiIntID(num);
+          IData dataServer = (IData)Activator.GetObject(
+              typeof(IData), url);
+          PadInt p = dataServer.createPadInt(2);
+          p.write(9999);
+
+          PadInt j = dataServer.acessPadInt(2);
+
+          Console.WriteLine(j.read());
+
+        }
+    }
+}
--- Projecto/Client/Client.cs
+++ Projecto/Client/Client.cs
@@ -11,53 +11,10 @@ using System.Collections;
 
 namespace PADIDSTM
 {
-    public class Client
-    {
-        static IMaster masterServer;
-        static ServerHashTable dataServersPorts;
-        
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Client port?");
-            int port = Convert.ToInt32(Console.ReadLine());
-            launchClient(port);
-            requestHash();
-            createPadInt();
-            Console.ReadLine();
-        }
+  public class Client
+  {
 
-        static void launchClient(int port)
-        {
-            TcpChannel channel = new TcpChannel(port);
-            ChannelServices.RegisterChannel(channel, true);
 
-            masterServer = (IMaster)Activator.GetObject(
-                typeof(IMaster),
-                "tcp://localhost:1000/MasterServer");
 
-        }
-
-        static void requestHash()
-        {
-            dataServersPorts = masterServer.requestHashTable();
-        }
-
-        static void createPadInt()
-        {
-            Random rnd = new Random();
-            int num = rnd.Next(0,666);
-            string url = dataServersPorts.getServerByPadiIntID(num);
-            IData dataServer = (IData)Activator.GetObject(
-                typeof(IData),url);
-            PadInt p = dataServer.createPadInt(2);
-            p.write(9999);
-
-            PadInt j = dataServer.acessPadInt(2);
-
-            Console.WriteLine(j.read());
-            
-        }
-
-    }
-
+  }
 }
